b57e397efd4f6f8085a2f200d720d96de480b472
patrick hevrdejs <nathanhevrdejs@gmail.com>
2019-12-04 10:37:19 -0600

    fixing push issues

---
+++ WebAPISample/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPISample.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        [DisplayName("Title")]
+        public string Title { get; set; }
+        [DisplayName("Genre")]
+        public string Genre { get; set; }
+        [DisplayName("Director")]
+        public string Director { get; set; }
+    }
+}
