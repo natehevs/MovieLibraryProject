44b7f9dbf63d8ff8e63d2adec3f42143cafd372e
patrick hevrdejs <nathanhevrdejs@gmail.com>
2019-12-05 09:30:07 -0600

    Put and Delete now work for MovieLibrary

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -35,14 +35,24 @@ namespace WebAPISample.Controllers
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public IHttpActionResult Put(int? id, [FromBody]Movie movie)
         {
-            // Update movie in db logic
+            Movie movieList = db.Movies.Where(h => h.MovieId == id).FirstOrDefault();
+            movieList.Title = movie.Title;
+            movieList.Genre = movie.Genre;
+            movieList.Director = movie.Director;
+            db.SaveChanges();
+            return Ok(movieList);
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        public IHttpActionResult Delete(int id)
         {
+            Movie movie = db.Movies.Where(m => m.MovieId == id).FirstOrDefault();
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            List<Movie> movieList = db.Movies.ToList();
+            return Ok(movieList);
             // Delete movie from db logic
         }
     }
